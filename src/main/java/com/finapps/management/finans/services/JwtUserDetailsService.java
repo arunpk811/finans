834ed7523e69dadd7c2e9b5587cb511fc7983903
Arun PK <64650271+arunpk811@users.noreply.github.com>
2020-06-01 15:43:30 +0530

    Bcrypted password added

--- src/main/java/com/finapps/management/finans/services/JwtUserDetailsService.java
+++ src/main/java/com/finapps/management/finans/services/JwtUserDetailsService.java
@@ -27,7 +27,7 @@ public class JwtUserDetailsService implements UserDetailsService {
 
 		try {
 			UserDetail user = userRepo.findByUsername(username);
-			if(username.equals("superadmin")) return new User(username,"P@ssw0rd1",new ArrayList<>());
+			if(username.equals("superadmin")) return new User(username,"$2a$10$d1mgtJ7yA8mFgp6FRTlfEug6jhlh4Ab8vYIaMJH6wFNaOM761xge2",new ArrayList<>());
 			if(user !=null && user.getIsActive() ==1) 
 				return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
 			else 
